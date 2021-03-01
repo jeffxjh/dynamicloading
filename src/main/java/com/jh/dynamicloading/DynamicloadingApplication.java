0e034c2d779a3ba0080d3e83f5ae3fc0476c31f5
xujiahao <xujiahao@qzdatasoft.com>
2021-03-01 17:33:12 +0800

    提交

--- src/main/java/com/jh/dynamicloading/DynamicloadingApplication.java
+++ src/main/java/com/jh/dynamicloading/DynamicloadingApplication.java
@@ -1,13 +1,14 @@
 package com.jh.dynamicloading;
 
 import cn.hutool.extra.spring.SpringUtil;
+import com.jh.dynamicloading.aspect.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Import;
 
-@Import({cn.hutool.extra.spring.SpringUtil.class})
+@Import({/*cn.hutool.extra.spring.SpringUtil.class,*/DynamicDataSourceRegister.class})
 @SpringBootApplication
 public class DynamicloadingApplication {
 
